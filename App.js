ce660dea72d901c70000f6257fbadf76531ded9e
Marta Obregon <marta.garcia.obregon@.com>
2020-12-01 18:01:13 +0000

    remove firebase and add localhost listening server

--- App.js
+++ App.js
@@ -4,32 +4,25 @@ import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import  Header  from './components/Header';
 import Post from './components/Post';
-import {db} from './firebase'
+
+import {API_URL} from './config'
+import axios from 'axios'
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-        imageUrl:'https://i.pinimg.com/originals/3e/72/6f/3e726f015975ebd9791a2eae433f4d17.jpg', 
-        username:"MarianRobinson",
-        caption:'a day in my life #loveicecream'
-    },
-    {
-        imageUrl:'https://i.pinimg.com/736x/ec/74/1e/ec741e60e001dbd763ebf869e5aa0c37.jpg',
-        username:"lifeonmars9",
-        caption:'<3'
-    },
-    {
-      imageUrl:'https://i.redd.it/gzcx7xvhzif31.jpg', 
-      username:"ElonMusk",
-      caption:'SpaceX is looking for world-class talent.'
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(()=>{
-    db.collection('posts').onSnapShot(snaphot =>{
-      setPosts(snaphot.docs.map((doc=> doc.data())))
+    
+    axios.get(`${API_URL}/getposts`)
+    .then((response)=>{
+      console.log(response.data)
+      
+      setPosts([...response.data])
+      
+      
     })
+    
   }, [])
 
   return (
@@ -40,7 +33,8 @@ function App() {
         <Header/>
         <ScrollView contentInset={{bottom: 160}} >
           {
-            posts.map((post)=>(<Post username={post.username}
+            posts.map((post, i)=>(<Post key={i}
+              username={post.username}
               imageUrl={post.imageUrl}
               caption = {post.caption}
             />))
